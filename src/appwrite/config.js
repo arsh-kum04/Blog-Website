ee1a6e8ec7f48b83f7ca9ff2391619af172671a1
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:33:32 +0530

    Add documentation for config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,22 +1,39 @@
+**Folder Name:** Appwrite
+
+**File Name:** service.js
+
+**Line by Line Documented Code:**
+```js
+// Import the conf file to get the Appwrite configuration
 import conf from '../conf/conf.js';
+
+// Import the Appwrite Client and various modules
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
+// Define the Service class
 export class Service {
-
+    // Create a new Appwrite Client using the configuration in the conf file
     client = new Client();
+    // Create a new Databases instance
     database;
+    // Create a new Storage instance
     bucket;
 
+    // Constructor to initialize the Appwrite Client and set the project and endpoint
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
+        // Initialize the Databases instance
         this.database = new Databases(this.client);
+        // Initialize the Storage instance
         this.bucket = new Storage(this.client);
     }
 
+    // Function to create a new post
     async createPost({ title, slug, content, featuredImage, status, userId }) {
         try {
+            // Create a new document in the posts collection using the given data
             return await this.database.createDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
@@ -30,13 +47,15 @@ export class Service {
                 }
             )
         } catch (error) {
+            // Log any errors that occur during the createPost function
             console.log("appwrite service :: createPost :: error", error);
         }
     }
 
-
+    // Function to update a post
     async updatePost(slug, { title, content, featuredImage, status }) {
         try {
+            // Update the document in the posts collection using the given data
             return await this.database.updateDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
@@ -49,95 +68,121 @@ export class Service {
                 }
             )
         } catch (error) {
+            // Log any errors that occur during the updatePost function
             console.log("appwrite service :: updatePost :: error", error);
         }
     }
 
+    // Function to delete a post
     async deletePost(slug) {
         try {
+            // Delete the document in the posts collection using the given slug
             await this.database.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
             )
+            // Return true if the post was deleted successfully
             return true
         } catch (error) {
+            // Log any errors that occur during the deletePost function
             console.log("appwrite service :: deletePost :: error", error);
+            // Return false if the post was not deleted successfully
             return false
         }
     }
 
+    // Function to get a post by its slug
     async getPost(slug) {
         try {
+            // Get the document in the posts collection using the given slug
             return await this.database.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
             )
+            // Return true if the post was found
             return true
         } catch (error) {
+            // Log any errors that occur during the getPost function
             console.log("appwrite service :: getPost :: error", error);
+            // Return false if the post was not found
             return false
         }
     }
 
+    // Function to get all posts
     async getPosts(queries = [Query.equal("status", "active")]) {
         try {
+            // Get all documents in the posts collection using the given queries
             return await this.database.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries
             )
         } catch (error) {
+            // Log any errors that occur during the getPosts function
             console.log("appwrite service :: getPosts :: error", error);
+            // Return false if no posts were found
             return false
         }
     }
 
-    //File upload Services
-
+    // Function to upload a file
     async uploadFile(file) {
         try {
+            // Create a new file in the bucket using the given file
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
-                ID.unique(),
+                ID.unique(), // Generate a unique ID for the file
                 file,
             )
-
         } catch (error) {
+            // Log any errors that occur during the uploadFile function
             console.log("appwrite service :: uploadFile :: error", error);
+            // Return false if the file was not uploaded successfully
             return false
         }
     }
 
+    // Function to delete a file
     async deleteFile(fileId) {
         try {
+            // Delete the file in the bucket using the given file ID
             await this.bucket.deleteFile(
                 conf.appwriteBucketId,
                 fileId,
             )
+            // Return true if the file was deleted successfully
             return true
         } catch (error) {
+            // Log any errors that occur during the deleteFile function
             console.log("appwrite service :: deleteFile :: error", error);
+            // Return false if the file was not deleted successfully
             return false
         }
     }
 
-
+    // Function to get a preview of a file
     getFilepreview(fileId) {
         try {
-            const res =  this.bucket.getFilePreview(
+            // Get a preview of the file in the bucket using the given file ID
+            const res = this.bucket.getFilePreview(
                 conf.appwriteBucketId,
                 fileId,
             )
-            // console.log("appwrite service :: getFilepreview :: res", res);
+            // Return the preview of the file
             return res;
         } catch (error) {
+            // Log any errors that occur during the getFilepreview function
             console.log("appwrite service :: getFilepreview :: error", error);
+            // Return false if the file preview could not be retrieved
             return false
         }
     }
 }
 
+// Create a new instance of the Service class and export it as default
 const service = new Service();
-export default service 
+export default service
+```
