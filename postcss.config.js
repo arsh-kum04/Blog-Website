68942b00e20a6979826639b5e3413ed7c236f8c4
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:32:35 +0530

    Add documentation for postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+```javascript
+// Folder Name: src
+// File Name: tailwind.config.js
+
+// This file is the configuration file for Tailwind CSS. It defines the settings and options for how Tailwind CSS will process your CSS.
+
+// The `plugins` property is an array of plugins that you want to use with Tailwind CSS. In this case, we are using the `tailwindcss` and `autoprefixer` plugins.
+
+// The `tailwindcss` plugin is the core plugin that provides all of the core features of Tailwind CSS.
+
+// The `autoprefixer` plugin automatically adds vendor prefixes to your CSS. This ensures that your CSS will work in all major browsers.
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+```
