b22001d34874fa6c5018f81af5279bd9439cfa0a
Arsh Kumar <arshkum18@gmail.com>
2024-05-03 09:07:02 +0530

    Add documentation for auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,43 +2,101 @@
 
 **File Name: AuthService.js**
 
+**Line 1: Appwrite configuration**
+
 ```javascript
 // Appwrite configuration
 import conf from '../conf/conf.js';
+```
+
+**Line 2: Import Appwrite Client, Account and ID modules**
+
+```javascript
 // Import Appwrite Client, Account and ID modules
 import { Client, Account, ID } from "appwrite";
+```
 
+**Line 4: AuthService class handles user authentication and management**
+
+```javascript
 // AuthService class handles user authentication and management
 export class AuthService {
+```
 
+**Line 6: Appwrite Client object**
+
+```javascript
     client; // Appwrite Client object
-    account; // Appwrite Account object
+```
 
-    // Constructor initializes the Appwrite Client and Account objects
+**Line 7: Appwrite Account object**
+
+```javascript
+    account; // Appwrite Account object
+```
+
+**Line 9: Constructor initializes the Appwrite Client and Account objects**
+
+```javascript
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl) // Set Appwrite endpoint URL
             .setProject(conf.appwriteProjectId) // Set Appwrite project ID
         this.account = new Account(this.client); // Create new Appwrite Account object
     }
+```
 
-    // Creates a new user account
+**Line 12: Creates a new user account**
+
+```javascript
     async createAccount({ email, password, name }) {
+```
+
+**Line 14: Creates a new user account**
+
+```javascript
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name); // Create a new user account
+```
+
+**Line 16: If account creation is successful**
+
+```javascript
             if (userAccount) { // If account creation is successful
+```
+
+**Line 17: Automatically log in the newly created user**
+
+```javascript
                 return this.login({ email, password }); // Automatically log in the newly created user
+```
+
+**Line 19: If account creation fails, return the error object**
+
+```javascript
             } else {
                 return userAccount // If account creation fails, return the error object
             }
+        ```
+
+**Line 21: Catch errors**
+
+```javascript
         } catch (error) {
             // console.log("appwrite service :: authenticaion error:: createAccount", error);
             throw error; // Rethrow the error to be handled by the caller
         }
-    }
+```
 
-    // Logs in a user
+**Line 24: Logs in a user**
+
+```javascript
     async login({ email, password }) {
+```
+
+**Line 26: Create an email session for the user**
+
+```javascript
         try {
             return await this.account.createEmailSession(email, password); // Create an email session for the user
 
@@ -46,34 +104,54 @@ export class AuthService {
             // console.log("appwrite service :: authenticaion error:: login", error);
             throw error; // Rethrow the error to be handled by the caller
         }
-    }
+```
 
-    // Gets the current logged in user
+**Line 31: Gets the current logged in user**
+
+```javascript
     async getCurrentUser() {
+```
+
+**Line 33: Get the current logged in user**
+
+```javascript
         try {
             return await this.account.get(); // Get the current logged in user
         } catch (error) {
             // console.log("appwrite service :: authenticaion error:: getCurrentuser", error);
-            
+```
+
+**Line 37: If there is no logged in user, return null**
+
+```javascript
         }
         return null; // If there is no logged in user, return null
-    }
+```
 
-    // Logs out the current user
+**Line 40: Logs out the current user**
+
+```javascript
     async logout() {
+```
+
+**Line 42: Delete all user sessions**
+
+```javascript
         try {
             return await this.account.deleteSessions(); // Delete all user sessions
         } catch (error) {
             // console.log("appwrite service :: authenticaion error:: logout", error);
         }
-    }
+```
 
+**Line 47: Create a new instance of the AuthService class**
 
-}
-
-// Create a new instance of the AuthService class
+```javascript
 const authService = new AuthService();
+```
 
-// Export the AuthService instance
+**Line 49: Export the AuthService instance**
+
+```javascript
 export default authService
 ```
